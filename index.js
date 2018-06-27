5eb2bd81e0a0911354b8c969b903d049952c0857
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-06-27 06:27:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ console.log(takeANumber(katzDeliLine,"Fred"))
 
 function takeANumber(obj,value){
   Object.assign(obj, { names: value })
-  return obj[names].length
+  console.log(obj)
+  return 
   
 }
